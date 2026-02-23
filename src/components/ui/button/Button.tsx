ccfaef215a628531bd6c0298a8a12679ae347000
Николай Мехедов <mehedov.dev@yandex.ru>
2026-02-23 16:45:07 +0300

    refactor(ui): enhance Button component with forwardRef and improved typing
    feat(ui): introduce HomeFolderElement with contextual navigation

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,28 +1,32 @@
 import { cn } from '@/utils/cn'
+import { ComponentPropsWithRef, forwardRef } from 'react'
 
-interface Props {
-	className?: string
+interface Props extends ComponentPropsWithRef<'button'> {
 	variant?: 'outline' | 'primary'
-	children?: React.ReactNode
 }
 
-const VARIANT = {
+const VARIANTS = {
 	outline: 'bg-transparent border-2 border-neutral-200',
 	primary:
 		'border border-gray-200 bg-neutral-50 transition hover:bg-neutral-100',
 }
 
-export function Button({ className, variant, children }: Props) {
-	return (
-		<button
-			className={cn(
-				'cursor-pointer flex items-center justify-center gap-2 px-5 py-2 text-md font-medium text-neutral-700 rounded-lg',
-				className,
-				variant === 'outline' && VARIANT.outline,
-				variant === 'primary' && VARIANT.primary
-			)}
-		>
-			{children}
-		</button>
-	)
-}
+export const Button = forwardRef<HTMLButtonElement, Props>(
+	({ className, variant = 'primary', children, ...props }, ref) => {
+		return (
+			<button
+				ref={ref}
+				className={cn(
+					'cursor-pointer flex items-center justify-center gap-2 px-5 py-2 text-md font-medium text-neutral-700 rounded-lg',
+					className,
+					VARIANTS[variant],
+				)}
+				{...props}
+			>
+				{children}
+			</button>
+		)
+	},
+)
+
+Button.displayName = 'Button'
---
+++ src/components/ui/home-folder-element/home-folder-element.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React, { forwardRef } from 'react'
+import { PopoverContent } from '../popover/popover-content'
+import { EllipsisVertical, FolderClosed } from 'lucide-react'
+import Link from 'next/link'
+import { Popover, PopoverContext } from '../popover/popover'
+import { cn } from '@/utils/cn'
+import { PopoverTrigger } from '../popover/popover-trigger'
+import { ContextMenuContent, FolderProps } from '../folder/folder'
+
+const HomeFolderElement = forwardRef<HTMLAnchorElement, FolderProps>(
+	({ className, name, ...props }, ref) => {
+		return (
+			<Popover>
+				<PopoverContext.Consumer>
+					{context => (
+						<>
+							<Link
+								ref={ref}
+								href={`/folders/${name
+									.toLowerCase()
+									.replace(/\s+/g, '-')
+									.replace(/[^a-z0-9-]/g, '')}`}
+								className={cn(
+									'flex justify-between items-center w-[256px] bg-neutral-50 rounded-lg border border-gray-200 px-4 py-3',
+									className,
+								)}
+								onContextMenu={e => {
+									e.preventDefault()
+									context?.setCoords({ x: e.clientX, y: e.clientY })
+									context?.setOpen(true)
+								}}
+								{...props}
+							>
+								<div className='flex items-center gap-2'>
+									<FolderClosed size={20} />
+									<span className='text-neutral-700 line-clamp-1'>{name}</span>
+								</div>
+								<PopoverTrigger>
+									<EllipsisVertical className='text-neutral-700' />
+								</PopoverTrigger>
+							</Link>
+							<PopoverContent isContextMenu>
+								<ContextMenuContent />
+							</PopoverContent>
+						</>
+					)}
+				</PopoverContext.Consumer>
+			</Popover>
+		)
+	},
+)
+
+HomeFolderElement.displayName = 'HomeFolderElement'
+export default HomeFolderElement
